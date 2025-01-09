44ce4aaf4f18a4e06dc651e3889387121500544c
Dr. Georg Hackenberg <ghackenberg@gmail.com>
2025-01-09 10:19:33 +0100

    add opc ua example

---
+++ Quellen/OPC-Basics-Client/Program.cs
@@ -0,0 +1,93 @@
+﻿using Opc.UaFx;
+using Opc.UaFx.Client;
+
+namespace OPC_Basics_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lokale Variablen definieren
+
+            OpcNodeInfo info;
+
+            OpcValue value;
+
+            OpcStatus status;
+
+            object[] result;
+
+            // OPC UA Client erstellen
+
+            using (var client = new OpcClient("opc.tcp://localhost:4840"))
+            {
+                // Connect
+                client.Connect();
+
+                // Browse nodes
+                info = client.BrowseNode(OpcObjectTypes.ObjectsFolder);
+                Print(info);
+
+                // Read node
+                value = client.ReadNode("ns=2;s=Machine/Running");
+                Console.WriteLine($"[opc-ua-client] Read node = {value}");
+
+                // Read node attribute
+                value = client.ReadNode("ns=2;s=Machine/Running", OpcAttribute.DisplayName);
+                Console.WriteLine($"[opc-ua-client] Read node display name = {value}");
+
+                // Write node
+                status = client.WriteNode("ns=2;s=Machine/Parameter", 0);
+                Console.WriteLine($"[opc-ua-client] Write node = {status}");
+
+                // Write node attribute
+                status = client.WriteNode("ns=2;s=Machine/Parameter", OpcAttribute.DisplayName, "Parameter");
+                Console.WriteLine($"[opc-ua-client] Write node display name = {status}");
+
+                // Read/write node
+                for (var index = 0; index < 10; index++)
+                {
+                    value = client.ReadNode("ns=2;s=Machine/Parameter");
+                    Console.WriteLine($"[opc-ua-client] Read node = {value}");
+
+                    if (value.Status.IsGood && value.DataType == OpcDataType.Int32)
+                    {
+                        status = client.WriteNode("ns=2;s=Machine/Parameter", "Test");//(int) value.Value + 1);
+                        Console.WriteLine($"[opc-ua-client] Write node = {status}");
+                    }
+
+                }
+
+                // Call method
+                result = client.CallMethod("ns=2;s=Machine", "ns=2;s=Machine/MethodA");
+                Console.WriteLine($"[opc-ua-client] Call method");
+
+                // Call method with output argument
+                result = client.CallMethod("ns=2;s=Machine", "ns=2;s=Machine/MethodB");
+                Console.WriteLine($"[opc-ua-client] Call method with output argument = {result[0]}");
+
+                // Call method with input/output argument
+                result = client.CallMethod("ns=2;s=Machine", "ns=2;s=Machine/MethodC", 10);
+                Console.WriteLine($"[opc-ua-client] Call method with input/output argument = {result[0]}");
+            }
+
+            // Wait for enter key
+            Console.Write("[opc-ua-client] Press enter to exit");
+            Console.ReadLine();
+        }
+
+        static void Print(OpcNodeInfo parent, int level = 0)
+        {
+            var space = new string(' ', level);
+            var id = parent.NodeId;
+            var name = parent.Attribute(OpcAttribute.DisplayName).Value;
+
+            Console.WriteLine($"[opc-ua-client]{space}{id} {name}");
+
+            foreach (var child in parent.Children())
+            {
+                Print(child, level + 1);
+            }
+        }
+    }
+}
---
+++ Quellen/OPC-Basics-Server/Program.cs
@@ -0,0 +1,87 @@
+﻿using Opc.UaFx;
+using Opc.UaFx.Server;
+
+namespace OPC_Basics_Server
+{
+    internal class Program
+    {
+        // Zufallszahlengenerator
+
+        private static Random RANDOM = new Random();
+
+        // Ordnerknoten
+
+        private static OpcFolderNode machine = new OpcFolderNode(new OpcName("Machine"));
+
+        // Variablenknoten
+
+        private static OpcDataVariableNode<bool> running = new OpcDataVariableNode<bool>(machine, "Running", true);
+        private static OpcDataVariableNode<int> version = new OpcDataVariableNode<int>(machine, "Version", 1);
+        private static OpcDataVariableNode<int> random = new OpcDataVariableNode<int>(machine, "Random", 0);
+        private static OpcDataVariableNode<int> parameter = new OpcDataVariableNode<int>(machine, "Parameter", 0);
+
+        // Methodenknoten
+
+        private static OpcMethodNode methodA = new OpcMethodNode(machine, new OpcName("MethodA"), new Action(MethodA));
+        private static OpcMethodNode methodB = new OpcMethodNode(machine, new OpcName("MethodB"), new Func<int>(MethodB));
+        private static OpcMethodNode methodC = new OpcMethodNode(machine, new OpcName("MethodC"), new Func<int, int>(MethodC));
+
+        // Hauptroutine
+
+        static void Main(string[] args)
+        {
+            // Rückrufmethoden für Variablenknoten setzen
+
+            parameter.WriteVariableValueCallback = WriteParameter;
+
+            // OPC UA Server starten und Werte der Variablenknoten zyklisch aktualisieren
+
+            using (var server = new OpcServer("opc.tcp://localhost:4840/", machine))
+            {
+                // OPC UA Server starten
+
+                server.Start();
+
+                Console.WriteLine("Server gestartet!");
+
+                // Werte der Variablenknoten zyklisch aktualisieren
+
+                while (true)
+                {
+                    random.Status.Update(OpcStatusCode.Good);
+                    random.Timestamp = DateTime.UtcNow;
+                    random.Value = RANDOM.Next();
+                    random.ApplyChanges(server.SystemContext);
+
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        // Rückrufmethoden für Variablenknoten
+
+        private static OpcVariableValue<object> WriteParameter(OpcContext context, OpcVariableValue<object> value)
+        {
+            Console.WriteLine($"Neuer Wert {value.Value}");
+            return value;
+            //return new OpcVariableValue<object>(parameter.Value, new DateTime(), new OpcStatus(OpcStatusCode.BadNotWritable));
+        }
+
+        // Methodenimplementierungen für Methodenknoten
+
+        private static void MethodA()
+        {
+            Console.WriteLine("Methode A aufgerufen!");
+        }
+        private static int MethodB()
+        {
+            Console.WriteLine("Methode B aufgerufen!");
+            return 0;
+        }
+        private static int MethodC(int param)
+        {
+            Console.WriteLine("Methode C aufgerufen!");
+            return param;
+        }
+    }
+}
